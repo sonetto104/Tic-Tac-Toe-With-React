96e76d5b8e7afa83bec75d05cf7c068a3cbff0ff
sonetto104 <peter_regan88@yahoo.com>
2023-10-11 10:18:10 +0000

    add logic for toggle function to show x or o icons

--- tic-tac-toe/src/components/TicTacToe/tic-tac-toe.jsx
+++ tic-tac-toe/src/components/TicTacToe/tic-tac-toe.jsx
@@ -1,15 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './tic-tac-toe.css'
 import circle_icon from '../assets/o.png'
 import x_icon from '../assets/x.png'
 
+let data = ['','','','','','','','',''];
+
 const TicTacToe = () => {
+
+    let [count, setCount] = useState(0);
+    let [lock, setLock] = useState(false);
+    const toggle = (e,num) => {
+        if (lock) {
+            return 0;
+        }
+        if (count%2===0) {
+            e.target.innerHTML = `<img src=${x_icon}>`;
+            data[num]='x';
+            setCount(++count);
+        }
+        else {
+            e.target.innerHTML = `<img src=${circle_icon}>`;
+            data[num]='o';
+            setCount(++count);
+        }
+
+    }
     return(
         <div className='container'>
             <h1 className='title'>Tic Tac Toe Game In <span>React</span></h1>
-            <div className='board'>
-
-            </div>
+                <div className='board'>
+                    <div className='row1'>
+                        <div className='boxes' onClick={(e)=>{toggle(e,0)}}></div>
+                        <div className='boxes' onClick={(e)=>{toggle(e,1)}}></div>
+                        <div className='boxes' onClick={(e)=>{toggle(e,2)}}></div>
+                    </div>
+                    <div className='row2'>
+                        <div className='boxes' onClick={(e)=>{toggle(e,3)}}></div>
+                        <div className='boxes' onClick={(e)=>{toggle(e,4)}}></div>
+                        <div className='boxes' onClick={(e)=>{toggle(e,5)}}></div>
+                    </div>
+                    <div className='row3'>
+                        <div className='boxes' onClick={(e)=>{toggle(e,6)}}></div>
+                        <div className='boxes' onClick={(e)=>{toggle(e,7)}}></div>
+                        <div className='boxes' onClick={(e)=>{toggle(e,8)}}></div>
+                    </div>
+                </div>
              <button className="reset">Reset</button>
         </div>
     )
